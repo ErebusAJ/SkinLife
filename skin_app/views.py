5417353b262c63b0f7d275389a7bbfd836586a53
Aarya <falconblaze90@gmail.com>
2024-02-23 05:17:15 +0530

    Added main_app page

--- skin_app/views.py
+++ skin_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http.response import responses
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
     return render(request, 'skin_app/home.html')
 
 
+@login_required()
 def dashboard(request):
     return render(request, 'skin_app/main_app.html')
